d232b86802e230d9e1cb9255b7dc270192385209
Jesse <jessemutua76@gmail.com>
2021-06-30 12:58:49 +0300

    Add information on Kenya

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-function dev(e, i) {
+function dev(e, i, f, g) {
 
     $(document).ready(function() {
         e.click(function() {
-            e.toggle();
+            e.toggle(1000);
             i.toggle(1000);
         });
         i.click(function() {
             e.toggle(1000);
             i.toggle(1000);
         });
+        f.click(function() {
+            f.toggle();
+            g.toggle(1000);
+        });
+        g.click(function() {
+            f.toggle(1000);
+            g.toggle(1000);
+        })
     });
 }
 dev($("#eastA"), $("#image1"));
+dev($(".kenyat"), $("#image2"))
