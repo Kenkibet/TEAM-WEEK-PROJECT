70e990f8d7a6ee24d83a08e48127c56a373f6a46
MarkYegon7 <mark.yegon@student.moringaschool.com>
2021-07-01 13:12:26 +0300

    pending changes

---
+++ js/mark.js
@@ -0,0 +1,50 @@
+function dev(i, p) {
+
+    $(document).ready(function() {
+        i.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+        p.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+    });
+}
+
+dev($("#south"), $("#imageon"));
+
+
+
+function dev(i, p) {
+
+    $(document).ready(function() {
+        i.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+        p.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+    });
+}
+
+dev($("#west2"), $("#imageon2"));
+
+
+function dev(i, p) {
+
+    $(document).ready(function() {
+        i.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+        p.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+    });
+}
+
+dev($("#north2"), $("#imageon3"));
